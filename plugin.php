ae226d8223a837f1170c07a2a9b21fb04b5515cd
jcopado <jcopado@surlabs.es>
2023-03-29 08:23:36 +0200

    Main ilias7

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "exmqst";
 
 // code version; must be changed for all code changes
-$version = "8.0.0";
+$version = "7.13.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "8.0";
-$ilias_max_version = "9.999";
+$ilias_min_version = "7.13";
+$ilias_max_version = "7.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann";
